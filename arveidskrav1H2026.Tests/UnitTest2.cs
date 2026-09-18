a2ffb9ee036fd7cf81d04e2e39e3f7f7fd8997cb
jakloop <jakobsorl@live.no>
2026-09-18 10:50:10 +0200

    Added test to CustomStack

--- arveidskrav1H2026.Tests/UnitTest2.cs
+++ arveidskrav1H2026.Tests/UnitTest2.cs
@@ -20,14 +20,37 @@ public class UnitTest2
     [Fact]
     public void CustomStack_PopReturnsTheLastValue()
     {
+        // arrange
         CustomStack<string> stack = new CustomStack<string>();
         stack.Push("Some");
         stack.Push("Body");
         stack.Push("Once");
         stack.Push("Told");
         stack.Push("Me");
-        
+        // act
         string result = stack.Pop();
+        // assert
         Assert.Equal("Me", result);
+    }
+    
+    [Fact]
+    public void CustomStack_ThrowsErrorWhenStackIsFull()
+    {
+        // arrange
+        CustomStack<string> stack = new CustomStack<string>();
+        for (int i = 0; i < 10; i++)
+        {
+            stack.Push("Hello");
+        }
+        Assert.Throws<IndexOutOfRangeException>(() => stack.Push("Goodbye"));
+    }
+
+    [Fact]
+    public void CustomStack_PopOnEmptyStack()
+    {
+        // arrange
+        CustomStack<string> stack = new CustomStack<string>();
+        // assert
+        Assert.Throws<IndexOutOfRangeException>(() => stack.Pop());
     }
 }
